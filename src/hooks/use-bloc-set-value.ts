3f4a53be02db25902eec75ace75d4123e3dd3976
Jacob Tipp <jacobtipp@gmail.com>
2022-12-14 14:17:44 -0800

    refactor(useSetValue): changed method name to useBlocSetValue

--- src/hooks/use-bloc-set-value.ts
+++ src/hooks/use-bloc-set-value.ts
@@ -2,7 +2,7 @@ import { ClassType, EmitUpdaterCallback, Cubit } from "@bloc-state/bloc"
 import { StateType, UseBlocConfig } from "../types"
 import { useBlocInstance } from "./use-bloc-instance"
 
-export const useSetBloc = <B extends Cubit<any>>(
+export const useBlocSetValue = <B extends Cubit<any>>(
   bloc: ClassType<B>,
   config?: UseBlocConfig,
 ): ReturnType<
--- tests/hooks/use-bloc-set-value.spec.ts
+++ tests/hooks/use-bloc-set-value.spec.ts
@@ -1,11 +1,11 @@
 import { cleanup, renderHook } from "@testing-library/react"
 import { cubitCounterWrapper as ccw } from "../test-helpers/wrappers"
-import { useSetBloc } from "../../src"
+import { useBlocSetValue } from "../../src"
 import { clearBlocContext } from "../../src/context/context"
 import CounterCubit from "../test-helpers/counter/counter.cubit"
 import { AwilixContainer, createContainer } from "awilix"
 
-describe("useSetBloc", () => {
+describe("useBlocSetValue", () => {
   let cubitCounterWrapper: ({ children }: any) => JSX.Element
   let container: AwilixContainer
 
@@ -22,7 +22,7 @@ describe("useSetBloc", () => {
 
   it("should return the emit method of a bloc", () => {
     expect.assertions(1)
-    const { result } = renderHook(() => useSetBloc(CounterCubit), {
+    const { result } = renderHook(() => useBlocSetValue(CounterCubit), {
       wrapper: cubitCounterWrapper,
     })
     expect(result.current).toBeInstanceOf(Function)
